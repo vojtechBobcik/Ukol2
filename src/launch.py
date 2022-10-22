ef08da0070387d9a0aab31481d114458a1a033b6
VojtechBobcik <xbobcik@mendelu.cz>
2022-10-22 19:36:07 +0200

    první úkol hotový. Použit Kruskalův alg.

--- src/launch.py
+++ src/launch.py
@@ -13,15 +13,41 @@ for x,y in inputRead.inputReadEdges().items():
     g.graphEdges.append(Edge(edgeFrom, edgeTo, y))
 
 for x in g.graphEdges:
-    print("hrana:"+repr(x.edgeFrom.nodeName))
-    print("hrana:"+repr(x.edgeTo.nodeName))
-    print("hodnota:"+repr(x.value))
-
     g.getNodeWithName(x.edgeFrom.nodeName).outputEdges.append(x)
     g.getNodeWithName(x.edgeTo.nodeName).inputEdges.append(x)
 
-for x in g.graphNodes:
-    print("jmeno uzlu " + x.nodeName)
-    print("vstup: " + repr(x.inputEdges))
-    print("vystup: " + repr(x.outputEdges))
+def sortEdgesByValue(x):
+    return x.value
+
+g.graphEdges.sort(key=sortEdgesByValue)
+
+processedNodes = []
+spanningTreeEdges = []
+for x in g.graphEdges:
+    print(x.value, x.edgeFrom.nodeName, x.edgeTo.nodeName)
+    if x.edgeFrom in processedNodes and x.edgeTo in processedNodes:
+        continue
+
+    if x.edgeFrom not in processedNodes and x.edgeTo not in processedNodes:
+
+        processedNodes.append(x.edgeFrom)
+        processedNodes.append(x.edgeTo)
+        spanningTreeEdges.append(x)
+        continue
+
+    if x.edgeFrom not in processedNodes:
+
+        processedNodes.append(x.edgeFrom)
+        spanningTreeEdges.append(x)
+        continue
+
+    if x.edgeTo not in processedNodes:
+        processedNodes.append(x.edgeTo)
+        spanningTreeEdges.append(x)
+        continue
+    
+print("Output:")
+for x in spanningTreeEdges:
+    print(repr(x.edgeFrom.nodeName) + " - " + repr(x.edgeTo.nodeName))
+
 
